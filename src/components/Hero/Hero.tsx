214d59316881dcc4f64e812d046ef253a17b3f38
Febry <146858262+alfatfebry@users.noreply.github.com>
2025-07-10 11:01:35 +0700

    Merge branch 'main' of https://github.com/alfatfebry/wph_portofolio
    # Conflicts:
    #	src/components/Hero/Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -29,43 +29,43 @@ export const Hero: React.FC = () => {
 
   return (
     <main>
-      <section id='home' className={styles.hero} ref={containerRef}>
+      <section id='home' className={Styles.hero} ref={containerRef}>
         <FadeWrapper
           type='end'
           direction='vertical'
-          className={styles.wavesWrapper}
+          className={Styles.wavesWrapper}
         >
-          <div className={styles.wavesBackground} />
+          <div className={Styles.wavesBackground} />
         </FadeWrapper>
 
-        <div className={styles.overflowWrapper}>
-          <div className={styles.container}>
+        <div className={Styles.overflowWrapper}>
+          <div className={Styles.container}>
             {/* Label */}
-            <div className={styles.label}>👨🏻 Febry Alfat Portfolio</div>
+            <div className={Styles.label}>👨🏻 Febry Alfat Portfolio</div>
 
             {/* Title */}
-            <h1 className={styles.title}>
+            <h1 className={Styles.title}>
               I am a{' '}
-              <span className={styles.highlight}>
+              <span className={Styles.highlight}>
                 {isMobile ? (
                   <>
-                    <span className={styles.box}>
-                      <p className={styles.boxText}>Front-</p>
-                      <span className={styles.cornerTopRight}></span>
-                      <span className={styles.cornerBottomLeft}></span>
+                    <span className={Styles.box}>
+                      <p className={Styles.boxText}>Front-</p>
+                      <span className={Styles.cornerTopRight}></span>
+                      <span className={Styles.cornerBottomLeft}></span>
                     </span>
-                    <span className={styles.box}>
-                      <p className={styles.boxText}>End Developer</p>
-                      <span className={styles.cornerTopRight}></span>
-                      <span className={styles.cornerBottomLeft}></span>
+                    <span className={Styles.box}>
+                      <p className={Styles.boxText}>End Developer</p>
+                      <span className={Styles.cornerTopRight}></span>
+                      <span className={Styles.cornerBottomLeft}></span>
                     </span>
                   </>
                 ) : (
                   <>
-                    <span className={styles.box}>
-                      <p className={styles.boxText}>Front-End Developer</p>
-                      <span className={styles.cornerTopRight}></span>
-                      <span className={styles.cornerBottomLeft}></span>
+                    <span className={Styles.box}>
+                      <p className={Styles.boxText}>Front-End Developer</p>
+                      <span className={Styles.cornerTopRight}></span>
+                      <span className={Styles.cornerBottomLeft}></span>
                     </span>
                   </>
                 )}
@@ -74,14 +74,14 @@ export const Hero: React.FC = () => {
             </h1>
 
             {/* Description */}
-            <p className={styles.description}>
+            <p className={Styles.description}>
               I am a Front-End Developer who loves to create beautiful and
               functional websites. I am also a tech enthusiast who loves to
               learn new things.
             </p>
 
             {/* CTA Button */}
-            <Button as='a' href='#contactMe' className={styles.ctaButton}>
+            <Button as='a' href='#contactMe' className={Styles.ctaButton}>
               Contact Me
             </Button>
           </div>
